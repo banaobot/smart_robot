ecea0ec7668873a3299c300de3f9165d3e7e1c8c
banaobot <banaobot.hub@gmail.com>
2026-04-17 11:05:36 +0530

    Action Server Added-CLI

--- smart_robot/navigation_node.py
+++ smart_robot/navigation_node.py
@@ -1,53 +1,58 @@
-from smart_robot_interfaces.action import MoveRobot
-
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
 
+from smart_robot_interfaces.action import MoveRobot
+import time
+
 class NavigationNode(Node):
 
-    def __init__(self):
-      super().__init__('navigation_node')
+  def __init__(self):
+    super().__init__('navigation_node')
 
-      self._action_server = ActionServer(self, MoveRobot, 'move_robot', self.execute_callback)
-      self.get_logger().info("Action Server Started")
+    self._action_server = ActionServer(self, MoveRobot, 'move_robot', self.execute_callback)
+    self.get_logger().info("Action Server Started")
 
-    def execute_callback(self, goal_handle):
+  def execute_callback(self, goal_handle):
 
-      duration = goal_handle.request.duration
+    duration = goal_handle.request.duration
 
-      # Reject condition
-      if duration <= 0:
-          self.get_logger().info("Invalid goal → rejecting")
-          goal_handle.abort()
-					
-          result = MoveRobot.Result()
-          result.success = False
-					
-          return result
-
-      # Accept goal
-      self.get_logger().info(f"Goal accepted: {duration}")
-      goal_handle.succeed()
-			
+    # Abort condition
+    if duration <= 0:
+      self.get_logger().info("Invalid goal: Duration must be positive. Aborting...")
+      goal_handle.abort()
+          
       result = MoveRobot.Result()
-      result.success = True
-
+      result.success = False
+          
       return result
+
+    # Execute goal
+    self.get_logger().info(f"Executing Goal: {duration}")
+    
+    time.sleep(duration)
+      
+    self.get_logger().info("Goal execution completed successfully.")
+    goal_handle.succeed()
+      
+    result = MoveRobot.Result()
+    result.success = True
+
+    return result
       
 def main(args=None):
-	rclpy.init(args=args)
-	node = NavigationNode()
-	try:
-		rclpy.spin(node)
-	except KeyboardInterrupt:
-		pass
-	finally:
-		node.destroy_node()
-		try:
-			rclpy.shutdown()
-		except:
-			pass
+  rclpy.init(args=args)
+  node = NavigationNode()
+  try:
+    rclpy.spin(node)
+  except KeyboardInterrupt:
+    pass
+  finally:
+    node.destroy_node()
+    try:
+      rclpy.shutdown()
+    except:
+      pass
 
 if __name__ == '__main__':
-    main()
+  main()
