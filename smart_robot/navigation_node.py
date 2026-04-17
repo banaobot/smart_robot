9e933f0be51d590af305e270357fabaa98f74bc7
banaobot <banaobot.hub@gmail.com>
2026-04-17 12:23:23 +0530

    Action Server with Feedback-CLI

--- smart_robot/navigation_node.py
+++ smart_robot/navigation_node.py
@@ -16,7 +16,7 @@ class NavigationNode(Node):
   def execute_callback(self, goal_handle):
 
     duration = goal_handle.request.duration
-
+      
     # Abort condition
     if duration <= 0:
       self.get_logger().info("Invalid goal: Duration must be positive. Aborting...")
@@ -29,8 +29,15 @@ class NavigationNode(Node):
 
     # Execute goal
     self.get_logger().info(f"Executing Goal: {duration}")
+    feedback_msg = MoveRobot.Feedback()
+    start_time = time.time()
     
-    time.sleep(duration)
+    while time.time() - start_time < duration:
+      
+      feedback_msg.time_elapsed = time.time() - start_time
+      goal_handle.publish_feedback(feedback_msg)
+    
+      time.sleep(1)  # Sleep for 1 second
       
     self.get_logger().info("Goal execution completed successfully.")
     goal_handle.succeed()
